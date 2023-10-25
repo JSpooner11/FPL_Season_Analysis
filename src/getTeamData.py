d5c0a352f980d2c38638fd15c4e0839cc3d3e9ab
Jacob <spooner.jacob11@gmail.com>
2023-10-25 13:12:53 -0400

    Add code to get current season_id

--- src/getTeamData.py
+++ src/getTeamData.py
@@ -7,9 +7,21 @@ r = requests.get(url)
 
 data = r.json()
 teams = data["teams"]
+events = data["events"]
+
+def get_season_dates():
+    gw1_event = events[0]
+    gw1_date = gw1_event["deadline_time"]
+    gw1_start_year = gw1_date[0:4]
+    gw1_end_year = int(gw1_date[2:4])+1
+    season_id = gw1_start_year + "-" + str(gw1_end_year)
+
+    return season_id
+
+season_id = get_season_dates().strip()
 
 def gather_team_data():
-    with open("data/2023-24/team.csv", 'w', newline='') as file:
+    with open("data/"+ season_id + "/team.csv", 'w', newline='') as file:
         header = ['id', 'name', 'strength', 'season']
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
@@ -22,8 +34,9 @@ def gather_team_data():
                 "id": id,
                 "name": team['name'],
                 "strength": team['strength'],
-                "season": '2023-24'
+                "season": season_id
             }
             writer.writerow(team_data)
+            
 
 gather_team_data()
